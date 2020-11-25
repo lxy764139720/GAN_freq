145e6d4f209230c0a951f79e8884ad3325ae7d90
lxy764139720 <764139720@qq.com>
2020-11-25 18:05:14 +0800

    CycleGAN test

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -8,7 +8,9 @@ def read_img(img_path):
     img = cv2.imread(img_path, 0)
     dft = cv2.dft(np.float32(img), flags=cv2.DFT_COMPLEX_OUTPUT)
     dftshift = np.fft.fftshift(dft)
-    amplitude = 20 * np.log(cv2.magnitude(dftshift[:, :, 0], dftshift[:, :, 1]))
+    magnitude = cv2.magnitude(dftshift[:, :, 0], dftshift[:, :, 1])
+    amplitude = 20 * np.log(magnitude)
+
     w = np.shape(amplitude)[0]
     h = np.shape(amplitude)[1]
     w_center = np.floor(w / 2)
@@ -21,20 +23,22 @@ def read_img(img_path):
             d = np.sqrt((i - w_center) ** 2 + (j - h_center) ** 2)
             H = 1 / (1 + (d0 / d) ** (2 * n))
             hpf_result[i, j] = H * amplitude[i, j]
+
+    # normalization = hpf_result / np.max(hpf_result) - np.min(hpf_result)
     # plt.subplot(121), plt.imshow(img, cmap='gray')
     # plt.title('original'), plt.axis('off')
     # plt.subplot(122), plt.imshow(amplitude, cmap='gray')
     # plt.title('dft'), plt.axis('off')
     # plt.show()
-    return hpf_result
+    return amplitude
 
 
 if __name__ == '__main__':
     work_path = os.getcwd()
     data_path = os.path.join(work_path, './airplane/1_fake')
     img_path = os.path.join(data_path, '00091.png')
-    fake_path = os.walk(r".\airplane\1_fake")
-    real_path = os.walk(r".\airplane\0_real")
+    fake_path = os.walk(r".\images\1_fake")
+    real_path = os.walk(r".\images\0_real")
     f_array = []
     r_array = []
     for path, dir_list, file_list in fake_path:
@@ -49,8 +53,10 @@ if __name__ == '__main__':
             r_array.append(read_img(file_path))
     f_ave = np.mean(np.array(f_array), axis=0)
     r_ave = np.mean(np.array(r_array), axis=0)
-    plt.subplot(121), plt.imshow(f_ave, cmap='gray')
+    im_color_f = cv2.applyColorMap(f_ave.astype(np.uint8), cv2.COLORMAP_JET)
+    im_color_r = cv2.applyColorMap(r_ave.astype(np.uint8), cv2.COLORMAP_JET)
+    plt.subplot(121), plt.imshow(im_color_r, cmap='gray')
+    plt.title('real'), plt.axis('off')
+    plt.subplot(122), plt.imshow(im_color_f, cmap='gray')
     plt.title('fake'), plt.axis('off')
-    plt.subplot(122), plt.imshow(r_ave, cmap='gray')
-    plt.title('original'), plt.axis('off')
     plt.show()
